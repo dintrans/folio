f0175cbfac4fe4b1325e821a69f576af94de7240
Gabriel Dintrans <dintrans.g@gmail.com>
2017-07-13 05:44:27 -0400

    commit 5.45am

--- vote/views.py
+++ vote/views.py
@@ -18,5 +18,16 @@ def checkTui(request, user_id, election_id, tui_number):
         data = serializers.serialize('json', Vote.objects.filter(voter__nroTui = tui_number))
     return HttpResponse(data, content_type='application/json')
 
+def checkRut(request, user_id, election_id, rut_number):
+    query = Vote.objects.filter(voter__rut = rut_number)
+    if not query:
+        q = Voter.objects.get(rut = rut_number)
+        data = serializers.serialize('json', [q])
+        tmp = json.loads(data)
+        data = json.dumps(tmp[0]['fields'])
+    else:
+        data = serializers.serialize('json', Vote.objects.filter(voter__rut = rut_number))
+    return HttpResponse(data, content_type='application/json')
+
 def getFolio(request, user_id, election_id, tui_number, folio):
     return HttpResponse("Ingreso folio %s" % folio )
