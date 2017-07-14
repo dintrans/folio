ae664bd93e937211e049d8d34e15c5085e13cc3b
Gabriel Dintrans <dintrans.g@gmail.com>
2017-07-13 22:34:14 -0400

    Second try

--- vote/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Voter, Election, Vote
-
-# Register your models here.
-
-admin.site.register(Voter)
-admin.site.register(Election)
-admin.site.register(Vote)
